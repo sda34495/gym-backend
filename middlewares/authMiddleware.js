d8f0ade5e14cbd2a15cb4da0d81557578e9c6c77
Danish Ali <sda34495@gmail.com>
2024-10-11 01:38:30 +0500

    changes

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,18 +1,17 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization').replace('Bearer ', '');
-
-  if (!token) {
-    return res.status(401).json({ message: 'Authorization denied, no token' });
-  }
+  let token = req.header("Authorization");
+  if (!token)
+    res.status(401).json({ message: "Authorization denied, no token" });
+  token = token.replace("Bearer ", "");
 
   try {
-    const decoded = jwt.verify(token, 'your_jwt_secret');  // Replace with your secret key
+    const decoded = jwt.verify(token, "your_jwt_secret"); // Replace with your secret key
     req.userId = decoded.userId;
     next();
   } catch (err) {
-    res.status(401).json({ message: 'Invalid token' });
+    res.status(401).json({ message: "Invalid token" });
   }
 };
 
