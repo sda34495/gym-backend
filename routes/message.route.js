197b343d5db2776b6d9427e854feca9fd17fe5ce
Mohsin <allen.clarke@softstandard.com>
2024-10-13 15:54:44 +0500

    changes

--- routes/message.route.js
+++ routes/message.route.js
@@ -4,10 +4,10 @@ const {
   getMessages,
   getMyMessages,
 } = require("../controllers/MessageController");
-const authMiddleware = require("../middlewares/authMiddleware");
+
 const router = express.Router();
 
-router.post("/", authMiddleware, sendMessage);
+router.post("/", sendMessage);
 router.get("/:userId", getMessages); // Retrieve messages for a user
 router.get("/my", getMyMessages); // Retrieve messages for a user
 
